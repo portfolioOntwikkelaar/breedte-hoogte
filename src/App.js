a3bf4dd2180278e5a0e012ee1888812b727aa453
noordseoostende <voropayev1@mail.ru>
2022-03-05 17:50:59 +0100

    nog de 3 afb toevoegen

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [weight, setWeight] = useState(0)
   const [height, setHeight] = useState(0)
   const [bmi, setBmi] = useState('')
-  const [message, setMessage] = useState('')
+  const [message, setMessage] = useState('empty string')
 
   let imgSrc = ''
 
@@ -19,6 +19,14 @@ function App() {
     } else {
       let bmi = (weight / (height * height) * 703)
       setBmi(bmi.toFixed(1))
+
+      if (bmi < 25) {
+        setMessage('You are underweight')
+      } else if (bmi>=25 &&  bmi < 30) {
+        setMessage('You are a healthy weight')
+      } else {
+        setMessage('You are overweight')
+      }
     }
   }
 
@@ -35,13 +43,13 @@ function App() {
           <form onSubmit={calcBmi} >
             <div>
               <label >
-                Weight(lbs)
+                Weight(lbs)(2.2046226218488kg)
               </label>
               <input value={weight} onChange={(e) => setWeight(e.target.value)} />
             </div>
             <div>
               <label >
-                Height(in)
+                Height(inches)(2.5cm)
               </label>
               <input value={height} onChange={(event) => setHeight(event.target.value)} />
             </div>
